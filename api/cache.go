c575deac24bcf9bb358ad325c019be4938443920
tristan <heytristaann@gmail.com>
2023-05-10 07:18:05 -0400

    speed improvement using goroutines

--- api/cache.go
+++ api/cache.go
@@ -2,30 +2,44 @@ package api
 
 // Import modules
 import (
+	"sync"
+
 	hermes "github.com/realTristan/Hermes/nocache"
 )
 
 // GetCourses() returns the courses from the cache
 func GetCourses(ft *hermes.FullText, query string, subject string) []map[string]interface{} {
-	// Search for query variable
-	var queryResult, _ = ft.Search(query, 100, false, map[string]bool{
-		"id":             false,
-		"components":     false,
-		"units":          false,
-		"description":    true,
-		"name":           true,
-		"pre_requisites": true,
-		"title":          true,
-	})
+	var (
+		wg            *sync.WaitGroup = &sync.WaitGroup{}
+		queryResult   []map[string]interface{}
+		subjectResult []map[string]interface{}
+	)
+
+	// Query the courses
+	wg.Add(1)
+	go func() {
+		queryResult, _ = ft.Search(query, 100, false, map[string]bool{
+			"id":             false,
+			"components":     false,
+			"units":          false,
+			"description":    true,
+			"name":           true,
+			"pre_requisites": true,
+			"title":          true,
+		})
+		wg.Done()
+	}()
 
 	// Search for the course title
-	if len(queryResult) < 100 {
-		var subjectResult, _ = ft.SearchValuesWithKey(subject, "title", 100)
-
-		// Return the two merged queries
-		return append(subjectResult, queryResult...)
+	if len(queryResult) <= 100 {
+		wg.Add(1)
+		go func() {
+			subjectResult, _ = ft.SearchValuesWithKey(subject, "title", 100)
+			wg.Done()
+		}()
 	}
+	wg.Wait()
 
-	// Return the query result
-	return queryResult
+	// Return the two merged queries
+	return append(subjectResult, queryResult...)
 }
